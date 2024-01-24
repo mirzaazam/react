8c1a95a27cf717584134489c2c3c35f12bae8095
mirza-azam <mirzaazam884@gmail.com>
2024-01-24 15:30:45 +0600

    create countries and flag

---
+++ api-rest-countries/js/countries.js
@@ -0,0 +1,24 @@
+
+const loadData = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => eachData(data))
+}
+
+const eachData = (countries) => {
+    countries.map(country => {
+        const container = document.getElementById('contriesContainer');
+        container.classList.add('container');
+        const creatEl = document.createElement('div');
+        creatEl.classList.add('createEl')
+        creatEl.innerHTML= 
+        ` 
+        <h2>${country.name.common}</h2> 
+        <img src="${country.flags.png}"></img>
+        `
+        console.log(country);
+        container.appendChild(creatEl);
+    })
+}
+
+loadData();
