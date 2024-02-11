6715d0391f2eb6d1093f0e903b11808a7b4611af
mirza-azam <mirzaazam884@gmail.com>
2024-02-11 12:13:49 +0600

    externaluser data load data

--- my-react2/src/App.js
+++ my-react2/src/App.js
@@ -1,28 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
 
 
-  function Counter() {
-    const [count, setCount] = useState(44)
-    const increase = () => setCount(count + 1) ;   
-  
-    return(
-      <div>
-        <h1>Counter: {count} </h1>
-        <button onClick={increase}>increse</button>
-        <button>decrese</button>
-      </div>
-    )
-  }
+function Counter() {
+  const [count, setCount] = useState(44)
+  const increase = () => setCount(count + 1);
+  const decrese = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>Counter: {count} </h1>
+      <button onClick={increase}>increse</button>
+      <button onClick={decrese}>decrese</button>
+    </div>
+  )
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect( () => {
+    
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+
+  }, []);
+}
 
 export default App;
