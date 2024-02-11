2f29d39dfed10627c57a947443968451d3c3a2e6
mirza-azam <mirzaazam884@gmail.com>
2024-02-12 00:11:02 +0600

    rest countrys & population

--- rest-countries-react/src/App.js
+++ rest-countries-react/src/App.js
@@ -8,22 +8,37 @@ function App() {
       <LoadCountries></LoadCountries>
     </div>
   );
+}
 
-  function LoadCountries() {
-    const [countries, setCountries] = useState([])
+function LoadCountries() {
+  const [countries, setCountries] = useState([])
 
-    useEffect(()=>{
-      fetch('https://restcountries.com/v3.1/all')
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
       .then(res => res.json())
-      .then(data => setCountries(data))
-    }, [])
-    return(
-      <div>
-        <h1>Load all countries</h1>
-        <h4>countries length: {countries.length} </h4>
-      </div>
-    )
-  }
+      .then(data => {
+        setCountries(data)
+        console.log(data)
+      })
+  }, [])
+  return (
+    <div>
+      <h1>Load all countries</h1>
+      <h4>countries length: {countries.length} </h4>
+      {
+        countries.map(country => <Show name={country.name.common} population={country.population}></Show>)
+      }
+    </div>
+  )
+}
+
+function Show(props) {
+  return(
+    <div>
+      <h1> {props.name} </h1>
+      <h1> {props.population} </h1>
+    </div>
+  )
 }
 
 export default App;
