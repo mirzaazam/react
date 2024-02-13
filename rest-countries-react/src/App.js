dcecbb7e6ddce7a3c32ff9870764a724c2c36c99
mirza-azam <mirzaazam884@gmail.com>
2024-02-13 21:30:08 +0600

    added components extension

--- rest-countries-react/src/App.js
+++ rest-countries-react/src/App.js
@@ -1,44 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Country from './components/Countries/Country';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+       <Country></Country>
     </div>
   );
 }
 
-function LoadCountries() {
-  const [countries, setCountries] = useState([])
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([])
 
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => {
-        setCountries(data)
-        console.log(data)
-      })
-  }, [])
-  return (
-    <div>
-      <h1>Load all countries</h1>
-      <h4>countries length: {countries.length} </h4>
-      {
-        countries.map(country => <Show name={country.name.common} population={country.population}></Show>)
-      }
-    </div>
-  )
-}
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res => res.json())
+//       .then(data => {
+//         setCountries(data)
+//         console.log(data)
+//       })
+//   }, [])
+//   return (
+//     <div>
+//       <h1>Load all countries</h1>
+//       <h4>countries length: {countries.length} </h4>
+//       {
+//         countries.map(country => <Show name={country.name.common} population={country.population}></Show>)
+//       }
+//     </div>
+//   )
+// }
 
-function Show(props) {
-  return(
-    <div>
-      <h1> {props.name} </h1>
-      <h1> {props.population} </h1>
-    </div>
-  )
-}
+// function Show(props) {
+//   return(
+//     <div>
+//       <h1> {props.name} </h1>
+//       <h1> {props.population} </h1>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ rest-countries-react/src/components/Countries/Country.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = () => {
+    return (
+        <div>
+            <h1>YHello</h1>
+        </div>
+    );
+};
+
+export default Country;<h1>YHello</h1>
